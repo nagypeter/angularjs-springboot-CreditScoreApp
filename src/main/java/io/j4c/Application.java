1ee9408aefa8e354aaacb42de95a3b4688b0e7ce
Peter Nagy <nagypeter76@gmail.com>
2018-05-31 14:19:48 +0200

    deployable to WebLogic 12c

--- src/main/java/io/j4c/Application.java
+++ src/main/java/io/j4c/Application.java
@@ -1,16 +1,29 @@
 package io.j4c;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
+@ComponentScan
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -26,4 +39,9 @@ public class Application {
 		};
 
 	}
+
+	@Override
+	public void onStartup(ServletContext container) {
+	}
+
 }
